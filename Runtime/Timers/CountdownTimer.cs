656e782ed11957158d1a7cc7a6e8f65363482976
Amaan12 <imamaan123@gmail.com>
2025-12-23 13:23:51 +0530

    Added Reverse In CountDownTimer

--- Runtime/Timers/CountdownTimer.cs
+++ Runtime/Timers/CountdownTimer.cs
@@ -7,16 +7,44 @@ namespace ImprovedTimers {
     public class CountdownTimer : Timer {
         public CountdownTimer(float value) : base(value) { }
 
-        public override void Tick() {
-            if (IsRunning && CurrentTime > 0) {
+        public override void Tick()
+        {
+            if (IsRunning && CurrentTime > 0 && !reversing)
+            {
                 CurrentTime -= Time.deltaTime;
             }
 
-            if (IsRunning && CurrentTime <= 0) {
+            if (IsRunning && CurrentTime <= 0 && !reversing)
+            {
+                CurrentTime = 0;
+                Stop();
+            }
+
+            if (IsRunning && CurrentTime < initialTime && reversing)
+            {
+                CurrentTime += Time.deltaTime;
+            }
+
+            if (IsRunning && CurrentTime >= initialTime && reversing)
+            {
+                CurrentTime = initialTime;
+                OnTimerReachedStart?.Invoke();
                 Stop();
             }
         }
 
+        /// <summary>
+        /// Allows reversing the CountdownTimer.
+        /// Helpful in case where you require holding a button and want to reset something by reversing the timer.
+        /// E.g. hold to interact, skip cutscene, etc.
+        /// </summary>
+        /// 
+        public Action OnTimerReachedStart = delegate { };
+        bool reversing = false;
+        public void Forward() => reversing = false;
+        public void Reverse() => reversing = true;
+
         public override bool IsFinished => CurrentTime <= 0;
+        public bool HasReachedStart => CurrentTime >= initialTime;
     }
 }
